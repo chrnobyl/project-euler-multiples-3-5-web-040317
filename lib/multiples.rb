179f9874c7a01fe3b26ceb3839023e56d324f3b2
github username <github email address>
2017-04-17 11:12:43 -0400

    Done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
 # Enter your procedural solution here!
+
+require 'pry'
+
+def collect_multiples(limit)
+  (1...limit).select do |x|
+  x % 3 == 0 || x % 5 == 0
+  end
+end
+
+collect_multiples(10)
+
+def sum_multiples(limit)
+  if limit == nil
+    raise ArgumentError
+  else
+    collect_multiples(limit).inject(0) do |y, x|
+      y + x
+    end
+  end
+end
+
+puts "hey"
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+
+require 'pry'
+class Multiples
+
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = (1...limit)
+  end
+
+  def collect_multiples
+    limit.select do |x|
+    x % 3 == 0 || x % 5 == 0
+    end
+  end
+
+  collect_multiples(10)
+
+  def sum_multiples
+    collect_multiples.inject(0) do |y, x|
+        y + x
+    end
+  end
+
+end
+
+puts "hey"
